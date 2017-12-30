d61162fc01371807439c61cc95400a7a2fb6a888
Isaac Esene <isaacesene@Isaacs-MBP.fios-router.home>
2017-12-29 20:15:19 -0500

    added .gitignore .gigattributes

--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TipCal
 //
-//  Created by Isaac Esene on 12/27/17.
+//  Created by Christian Ogbeifun on 12/27/17.
 //  Copyright © 2017 Christian Ogbeifun. All rights reserved.
 //
 
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages [tipControl.selectedSegmentIndex]
+        let total = bill * tip
+    
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
